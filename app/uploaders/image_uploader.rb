ec5e80189223636a23cc4fa8ee182d7e10828292
keiko <keikomakiba@gmail.com>
2018-12-02 15:07:04 +0000

    JS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,9 +2,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
    include CarrierWave::MiniMagick
+   
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  process :resize_to_fill => [400, 400] 
+  
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
     root to: 'top#index'
+    
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+    
 
     resources :blogs do 
       collection do 
